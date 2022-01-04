11d9c7f54205e18b00d8301b159269fbe5e9615e
aegis886 <aegis886@163.com>
2022-01-04 06:03:20 +0000

    https://github.com/aegis886/aaaa

---
+++ 1/push.bat
@@ -0,0 +1,9 @@
+for /l %%i in (1,1,10) do (
+　REM 产生[min,max]之间的随机数
+　set /a r=!random!%%!mod!+!min!
+　　echo.
+　echo 随机数%%i：!r!>1.txt
+)
+git add .
+git commit -m "https://github.com/aegis886/aaaa"
+git push
